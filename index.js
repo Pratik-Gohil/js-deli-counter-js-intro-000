8a23a4e94b5eff2b9e3605ca2a117a8183771cde
Pratik <pr.tikgohil@gmail.com>
2020-02-22 19:59:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-function takeANumber(array, name) {
-  array.push(name)
-  return `Welcome, ${name}. You are number ${array.length} in line.`
+function takeANumber(line, name) {
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 }
-function nowServing(array) {
-  if (array.length == 0) {
+function nowServing(line) {
+  if (line.length == 0) {
     return "There is nobody waiting to be served!"
   } else {
-     return `Currently serving ${array.shift()}.`
+     return `Currently serving ${line.shift()}.`
+  }
+}
+function currentLine(line) {
+  if (line.length == 0) {
+    return "The line is currently empty."
+  } else {
+    line.each((person, line) => {
+      return `The line is currently: ${line+1}. ${person}`
+    })
   }
 }
