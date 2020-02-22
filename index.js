1e18eac44464d99fae00781fade004ce38a213d0
Pratik <pr.tikgohil@gmail.com>
2020-02-22 19:53:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ function takeANumber(array, name) {
   return `Welcome, ${name}. You are number ${array.length} in line.`
 }
 function nowServing(array) {
-
+  if (array.length == 0) {
+    return "The line is currently empty."
+  } else {
+     return array.shift()
+  }
 }
