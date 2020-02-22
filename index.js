c0fb612dd5f2bf1348c141c72249bbf4cf6925a0
Pratik <pr.tikgohil@gmail.com>
2020-02-22 19:55:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function nowServing(array) {
   if (array.length == 0) {
     return "There is nobody waiting to be served!"
   } else {
-     return `Currently serving ${array.shift()}`
+     return `Currently serving ${array.shift()}.`
   }
 }
