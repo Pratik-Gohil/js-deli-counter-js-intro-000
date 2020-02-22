f317d509e8aef8fd9eac4393316853e16d362075
Pratik <pr.tikgohil@gmail.com>
2020-02-22 19:54:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function takeANumber(array, name) {
 }
 function nowServing(array) {
   if (array.length == 0) {
-    return "The line is currently empty."
+    return "There is nobody waiting to be served!"
   } else {
      return array.shift()
   }
